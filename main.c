891693b74e1c3d6a72270219ca5085507e2c90e2
Aphiwat Noiluea <f_400842@hotmail.com>
2020-10-04 15:11:11 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  int i;
+  float Sum=0;
+  for (i=1;i<=100;i++)
+    Sum = Sum + (i*i);
+  printf("Sum of 1 squares to 100 squares is %f\n",Sum);
+  getchar();
+  return 0;
+}
